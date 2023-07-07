2a66d50ceb10113e90f63ffb85ed9482d81f4cb5
NuB469 <111990473+NuB469@users.noreply.github.com>
2023-07-07 16:08:20 +0530

    Update Solved.py

--- Solved.py
+++ Solved.py
@@ -1,136 +1,93 @@
 import requests
-import csv
 from bs4 import BeautifulSoup
+import csv
 
-
-# import os
-
-def scrape_product_listings(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    product_list = soup.find_all('div', {'data-component-type': 's-search-result'})
-
-    products = []
-    for product in product_list:
-        product_data = {}
-
-        # Extract product URL
-        link = product.find('a', {'class': 'a-link-normal s-no-outline'})
-        if link:
-            product_data['URL'] = 'https://www.amazon.in' + link['href']
-
-        # Extract product name
-        name = product.find('span', {'class': 'a-size-medium a-color-base a-text-normal'})
-        if name:
-            product_data['Name'] = name.text.strip()
-
-        # Extract product price
-        price = product.find('span', {'class': 'a-price-whole'})
-        if price:
-            product_data['Price'] = price.text.strip()
-
-        # Extract rating
-        rating = product.find('span', {'class': 'a-icon a-icon-star-small a-star-small-4 aok-align-bottom'})
-        if rating:
-            product_data['Rating'] = rating.text.split()[0]
-
-        # Extract number of reviews
-        reviews = product.find('span', {'class': 'a-size-base s-underline-text'})
-        if reviews:
-            product_data['Reviews'] = reviews.text.strip()
-
-        products.append(product_data)
-        print(product_data)
-
-    return products
-
-
+# Function to scrape product details from a given URL
 def scrape_product_details(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    product_details = {}
+    # Check if the page is a valid product page
+    if soup.find("span", class_="a-size-medium"):
+        # Extract product details
+        product_title_element = soup.find("span", class_="a-size-medium")
+        product_title = product_title_element.text.strip() if product_title_element else ""
 
-    # Extract description
-    description = soup.find('div', {'id': 'productDescription'})
-    if description:
-        product_details['Description'] = description.text.strip()
+        product_price_element = soup.find("span", class_="a-offscreen")
+        product_price = product_price_element.text.strip() if product_price_element else ""
 
-    # Extract ASIN
-    asin = soup.find('th', string='ASIN')
-    if asin:
-        product_details['ASIN'] = asin.find_next('td').text.strip()
+        product_rating_element = soup.find("span", class_="a-icon-alt")
+        product_rating = product_rating_element.text.strip() if product_rating_element else ""
 
-    # Extract product description
-    prod_description = soup.find('div', {'id': 'feature-bullets'})
-    if prod_description:
-        desc_list = prod_description.find_all('span', {'class': 'a-list-item'})
-        product_details['Product Description'] = '\n'.join([desc.text.strip() for desc in desc_list])
+        product_reviews_element = soup.find("span", class_="a-size-base")
+        product_reviews = product_reviews_element.text.strip() if product_reviews_element else ""
 
-    # Extract manufacturer
-    manufacturer = soup.find('a', {'id': 'bylineInfo'})
-    if manufacturer:
-        product_details['Manufacturer'] = manufacturer.text.strip()
+        # Additional scraping within the product page (if required)
+        product_description = ""
+        product_asin = ""
+        product_manufacturer = ""
 
-    print(product_details)
-    return product_details
+        return {
+            "Title": product_title,
+            "Price": product_price,
+            "Rating": product_rating,
+            "Reviews": product_reviews,
+            "Description": product_description,
+            "ASIN": product_asin,
+            "Product Description": product_description,
+            "Manufacturer": product_manufacturer,
+        }
 
+    # Return None for non-product pages
+    return None
 
-def fetch_product_details(product_url):
-    # Code to fetch and extract product details from the provided URL
-    # Implement your logic here to extract the necessary information
-    # Return a dictionary containing the fetched details
+# Base URL for scraping product listing pages
+base_url = "https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_{}"
 
-    # Example implementation:
-    product_details = {
-        'Product URL': product_url,
-        'Product Name': 'Sample Product Name',
-        'Product Price': 'Sample Price',
-        'Rating': 'Sample Rating',
-        'Number of Reviews': 'Sample Number of Reviews'
-    }
-    return product_details
+# Number of pages to scrape (at least 20 in this case)
+num_pages = 20
 
+# List to store all scraped product details
+all_products = []
 
-def export_to_csv(data, filename):
-    fieldnames = ['Product URL', 'Product Name', 'Product Price', 'Rating',
-                  'Number of Reviews']  # Adjust the fieldnames as needed
+# Scrape product listing pages
+for page in range(1, num_pages + 1):
+    url = base_url.format(page)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    products = soup.find_all("div", class_="sg-col-inner")
 
-    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+    # Iterate over products on the current page
+    for product in products:
+        product_url_element = product.find("a", class_="a-link-normal")
+        if product_url_element:
+            product_url = "https://www.amazon.in" + product_url_element["href"]
+            print("Scraping:", product_url)  # Print the URL being scraped
+            product_details = scrape_product_details(product_url)
+            if product_details:
+                all_products.append(product_details)
 
-        for product_url in data:
-            product_details = fetch_product_details(product_url)
-            writer.writerow(product_details)
-            print(product_details)
+        # Limit the number of product URLs to be scraped
+        if len(all_products) >= 200:
+            break
 
+    # Break the outer loop if the limit of product URLs is reached
+    if len(all_products) >= 200:
+        break
 
-if __name__ == '__main__':
-    base_url = 'https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_{}'
-    num_pages = 20  # Number of pages
+# Export scraped data to a CSV file
+fieldnames = [
+    "Title",
+    "Price",
+    "Rating",
+    "Reviews",
+    "Description",
+    "ASIN",
+    "Product Description",
+    "Manufacturer",
+]
 
-    all_products = []
-    for page in range(1, num_pages + 1):
-        url = base_url.format(page)
-        print(f"Scraping products from page {page}...")
-        products = scrape_product_listings(url)
-        all_products.extend(products)
-
-    print("Scraping complete.")
-    print(f"Total products scraped: {len(all_products)}")
-
-    scraped_product_details = []
-    for product in all_products:
-        product_url = product['URL']
-        print(f"Fetching details for product: {product['Name']}")
-        details = scrape_product_details(product_url)
-        scraped_product_details.append(details)
-
-    print("Fetching details complete.")
-
-    export_to_csv(all_products, 'output.csv')
-    print("Data exported to CSV file.")
+with open("bag_data.csv", "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(all_products)
